3f7706d9e1b43e6678568ce78ee30812e61d67bf
Salman Toha <ishrak1846@gmail.com>
2026-06-20 14:49:42 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
 
   // ✅ Allow any image link (including Cloudinary)
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
